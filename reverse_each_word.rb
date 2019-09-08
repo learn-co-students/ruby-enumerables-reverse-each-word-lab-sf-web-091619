5d35982c1fb1915a55cf66c42e5ed730a08303b7
Urfan Guliyev <urfan196@yahoo.com>
2019-09-08 18:27:28 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,12 @@
 
 def reverse_each_word (sentence1)
-reverse_array = %w sentence1
-reverse_array.each {|word| word.reverse}
+%w sentence1.each {|word| word.reverse}
 end
 
 def reverse_each_word (sentence2)
-reverse_array = %w sentence2
-reverse_array.each {|word| word.reverse}
+%w sentence2.each {|word| word.reverse}
 end
 
 def use_collect (array)
-  reverse_array = %w array
-  reverse_array.collect {|word| word.reverse}
+%w array.collect {|word| word.reverse}
 end
